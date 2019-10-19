b82f836feee4fc4cbd870bd6fdfce7b4aa6734e5
Meruman <36783697+Meruman@users.noreply.github.com>
2019-10-19 19:01:54 +0200

    Basics and movement

---
+++ Basics unity/Moving/MovingBasic/Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start() 
+    {
+    }
+
+//it is better for movementes in lights
+    private void LateUpdate() 
+    {
+        //To avoid inconcistencies in the speed due to updates we use time.deltaTime
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
