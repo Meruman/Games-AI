a2d53da4fd0722c695897f57e504f96e683d0966
Martin MeRu <36783697+Meruman@users.noreply.github.com>
2022-02-04 17:52:28 +0100

    ANN
    ANN scripts to train an ANN and get an exor or exand classifier

---
+++ ANN/Assets/Layer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer 
+{
+    public int numNeurons;
+    public List<Neuron> neurons = new List<Neuron>();
+
+    public Layer(int nNeurons, int numNeuronInputs)
+    {
+        numNeurons = nNeurons;
+        for (int i = 0; i<numNeurons; i++)
+        {
+            neurons.Add(new Neuron(numNeuronInputs));
+        }
+    }
+}
